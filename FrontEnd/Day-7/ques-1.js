3ead3cb4d038eb3041387ed646c122188bcbb9be
sandeep-07 <sandeepisthename007@gmail.com>
2021-06-17 23:34:58 +0530

    tha-7 2questions left

---
+++ FrontEnd/Day-7/ques-1.js
@@ -0,0 +1,12 @@
+/* Write a JavaScript program to list the properties of a 
+JavaScript object. Sample object: 
+var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; 
+Sample Output: name,sclass,rollno*/
+
+let student={
+    firstName:"Sandeep",
+    sclass:"BTech",
+    rollno:"41",
+}
+
+console.log(student)
---
+++ FrontEnd/Day-7/ques-2.js
@@ -0,0 +1,9 @@
+let student={
+    firstName:"Sandeep",
+    sclass:"BTech",
+    rollno:"41",
+}
+
+console.log(student)
+delete student.rollno
+console.log(student)
