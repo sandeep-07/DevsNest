53015e753f96ba3aae95b164009201f9ea627938
sandeep-07 <sandeepisthename007@gmail.com>
2021-07-03 00:21:38 +0530

    Sundarikaran

--- FrontEnd/Day-14/script.js
+++ FrontEnd/Day-14/script.js
@@ -1,5 +1,5 @@
 function focusevent() {
-    document.querySelector('.name').style.backgroundColor = "blue"
+    document.querySelector('.name').style.backgroundColor = "alicblue"
 }
 
 function changeevent() {
@@ -7,8 +7,9 @@ function changeevent() {
     var value = e.options[e.selectedIndex].value;
 
     var text = e.options[e.selectedIndex].text;
-    console.log(text)
-    document.querySelector('.test').innerHTML = `You chose ${text} branch`
+    // console.log(text)
+
+    notify(`You chose ${text} branch`)
 }
 
 function clickevent() {
@@ -21,4 +22,21 @@ function submittt(e) {
     // console.log(event)
     console.log(e)
     e.preventDefault()
+    notify("Thanks for filling the form")
+}
+
+function notify(text) {
+
+    Toastify({
+        text: text,
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        onClick: function () {} // Callback after click
+    }).showToast();
 }
